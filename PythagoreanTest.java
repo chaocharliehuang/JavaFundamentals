46f7671a765f1f8675e55e1e52ec90db14c0fda6
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-31 13:11:00 -0700

    added Pythagorean Theorem assignment

---
+++ PythagoreanTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean pythagObj = new Pythagorean();
+        System.out.println(pythagObj.calculateHypotenuse(3, 4));
+    }
+}
