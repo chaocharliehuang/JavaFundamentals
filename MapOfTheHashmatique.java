d1eb7f675a36d716c768fa8fcf54b1dd05159729
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-31 17:38:34 -0700

    fixed Map of the Problematique assignment

--- MapOfTheHashmatique.java
+++ MapOfTheHashmatique.java
@@ -1,26 +1,15 @@
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 
 public class MapOfTheHashmatique {
     public static void main(String[] args) {
-        Map<String, String> trackList = new HashMap<String, String>();
+        HashMap<String, String> trackList = new HashMap<>();
         trackList.put("Stockholm Syndrome", "This is the last time I'll abandon you");
         trackList.put("Time is Running Out", "Our time is running out");
         trackList.put("Apocalypse Please", "Declare this an emergency");
         trackList.put("Hysteria", "I want it now");
         System.out.println(trackList.get("Stockholm Syndrome"));
-        for (Map.Entry<String, String> entry : trackList.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
+        for (String key : trackList.keySet()) {
+            System.out.println(key + " : " + trackList.get(key));
         }
     }
-
-    // public HashMap createTrackList() {
-    //     Map<String, String> trackList = new HashMap<String, String>();
-    //     trackList.put("Stockholm Syndrome", "This is the last time I'll abandon you");
-    //     trackList.put("Time is Running Out", "Our time is running out");
-    //     trackList.put("Apocalypse Please", "Declare this an emergency");
-    //     trackList.put("Hysteria", "I want it now");
-    //     return trackList;
-    // }
 }
